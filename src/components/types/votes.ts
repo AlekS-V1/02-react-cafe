8cfc13f67a1cdd80d248968b0fe43963775a6c15
OlexandrVakulin <vlipoq@gmail.com>
2025-08-30 08:11:17 +0200

    add htask files

---
+++ src/components/types/votes.ts
@@ -0,0 +1,12 @@
+export interface Votes {
+    good: number;
+    neutral: number;
+    bad: number;
+}
+export type VoteType = 'good' | 'neutral' | 'bad';
+
+
+
+// export default function Votes({ good, neutral, bad }: Votes) {
+    
+// }
